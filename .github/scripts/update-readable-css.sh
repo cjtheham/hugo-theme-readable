42c851a15d3d68a837b9e226c2e06dc4a8bf6a0a
Robert Lützner <robert.luetzner@pm.me>
2023-05-03 22:25:04 +0200

    CI: Add GitHub workflow to check for readable.css updates
    This adds a workflow and accompanying script to update readable.css. The
    script gets the latest tag version from the readable.css repository on
    codeberg.org and compares it to the currently used version. If the
    versions don't match up, we download readable.css and readable.min.css
    from Codeberg and update layouts/partials/head.html.
    
    I initially didn't bother to check the current version, because
    ultimately we only do 3 requests against Codeberg every time the
    workflow executes. However, I'm expecting this workflow to run once a
    day currently. If someone were to increase the frequency significantly,
    this might become problematic for Codeberg. It's easy enough to
    optimize, so we should do it.
    
    The workflow uses 'peter-evans/create-pull-request@v5', an action that
    allows us to create GitHub pull requests on the repo.
    
    The action seems really thought through to me. I checked that an
    existing pull request is not duplicated. In case the 'main' branch
    changes in between, it will simply force push changes to the existing
    pull request. This also means that we won't get flooded with pull
    requests for the same version update.
    
    If no changes are found, then the action will not open up a pull request
    at all.
    
    The repository's settings need to be adjusted to allow workflows to
    create pull requests. This can be done in GitHub, by opening up the
    repository's 'Settings', clicking 'Actions' and 'General' and updating
    'Workflow permissions' to allow 'Read and write permissions' and 'Allow
    GitHub Actions to create and approve pull requests'.
    
    Other than that, there's nothing to do to enable this workflow. Once
    this commit is merged, the workflow will start it's scheduled execution.
    
    The only thing I couldn't seem to get working is a notification for the
    newly created pull request. I'm not sure if I was simply unable to get
    it to work with a fork, or what's the reason behind this. I would have
    expected an email at least. However, I think this can be optimized in
    a future commit.

---
+++ .github/scripts/update-readable-css.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# Query the codeberg.org API (see
+# https://codeberg.org/api/swagger#/repository/repoListReleases) and use jq to
+# get the tag name of the latest release (i.e. the first element of the array
+# in the JSON output). Finally, use sed to remove the double quotes around the
+# value.
+TAG_VERSION=$(curl -s \
+    -H 'accept: application/json' \
+    https://codeberg.org/api/v1/repos/Freedom-to-Write/readable.css/releases \
+    | jq '.[0].tag_name' \
+    | sed 's/"//g')
+
+echo "Latest tag: ${TAG_VERSION}"
+
+# Get the current version.
+# The '-o' option will only output the match grep found.
+# We look for readable.min.css and a set of three numbers seperated by dots,
+# i.e. a semantic version. readable.min.css is used to make sure that we match
+# the right numbers in the file.
+# We use sed to get rid of the readable.min.css prefix.
+CURRENT_VERSION=$(grep -o \
+    'readable.min.css?v=[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+' \
+    layouts/partials/head.html \
+    | sed 's/readable.min.css?v=/v/')
+echo "Current version: ${CURRENT_VERSION}"
+
+if [[ $TAG_VERSION == $CURRENT_VERSION ]]
+then
+    echo "Nothing to do. The current version is already up to date."
+else
+    curl -s "https://codeberg.org/Freedom-to-Write/readable.css/raw/tag/${TAG_VERSION}/readable.css" > static/css/readable.css
+    curl -s "https://codeberg.org/Freedom-to-Write/readable.css/raw/tag/${TAG_VERSION}/readable.min.css" > static/css/readable.min.css
+    VERSION=$(echo "${TAG_VERSION}" | sed 's/v//')
+    sed -i "s/readable.min.css?v=.*/readable.min.css?v=${VERSION}\">/" layouts/partials/head.html
+fi
+
+# Add the latest tag version to the workflow environment, so we can access
+# it in later steps.
+echo "READABLE_CSS_TAG=${TAG_VERSION}" >> "$GITHUB_ENV"
